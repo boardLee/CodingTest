a469ccd6df2795ee168a962f2f6dae8acc9d31f0
MHLEE12 <coolmh12@gmail.com>
2023-03-14 21:54:51 +0900

    230314_LeetCode
    easy 문제

---
+++ MH/Solution230314.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Solution230314 {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(result);
+        
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] ex = {-4,-1,0,3,10};
+        int[] result1 = new int[ex.length];
+
+        Solution230314 s = new Solution230314();
+        result1 = s.sortedSquares(ex);
+
+        for(int i = 0; i < ex.length; i++) {
+            System.out.println(i + " : " + result1[i]);
+
+        }
+    }
+}
