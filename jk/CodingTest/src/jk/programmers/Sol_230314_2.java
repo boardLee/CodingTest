7d64da37d0371f29a4250caae926ec48ecc24d46
Jung-Kwon Lee <akdirdl@gmail.com>
2023-03-14 23:46:36 +0900

    20230314-프로그래머스-나이 출력20230314-프로그래머스-나이 출력

---
+++ jk/CodingTest/src/jk/programmers/Sol_230314_2.java
@@ -0,0 +1,17 @@
+package jk.programmers;
+
+/**
+ * 프로그래머스 - 나이 출력
+ * [https://school.programmers.co.kr/learn/courses/30/lessons/120820]
+ */
+public class Sol_230314_2 {
+    public static int solution(int age) {
+
+        return 2023-age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(40));
+    }
+
+}
