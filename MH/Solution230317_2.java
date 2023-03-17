3251e5d9734377879cebb683e38b55ec5f062e03
MHLEE12 <coolmh12@gmail.com>
2023-03-17 22:51:27 +0900

    230317_두번째 문제 url 추가

--- MH/Solution230317_2.java
+++ MH/Solution230317_2.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Solution230317_2 {
+	
+	// 직사각형 별찍기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12969
+	
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
