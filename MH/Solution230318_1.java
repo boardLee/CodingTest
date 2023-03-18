fe2fce229e5ce42021ff8e59bc6be7e3b83b4b06
MHLEE12 <coolmh12@gmail.com>
2023-03-18 17:21:50 +0900

    230318 프로그래머스 4
    lv1

---
+++ MH/Solution230318_1.java
@@ -0,0 +1,25 @@
+public class Solution230318_1 {
+	
+	// 프로그래머스 - lv.1 - 약수의 합
+	public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++) {
+        	if(i * i == n) {
+        		answer += i;
+        	} else if(n % i == 0) {
+        		answer += i;
+        	}
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int ex1 = 12;
+		int ex2 = 5;
+		
+		Solution230318_1 s = new Solution230318_1();
+		
+		System.out.println(s.solution(ex2));
+	}
+}
