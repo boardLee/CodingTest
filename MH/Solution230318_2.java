717d12d9e2a23bcb5fe87e2ac5e66699bee12dff
MHLEE12 <coolmh12@gmail.com>
2023-03-18 17:01:58 +0900

    230318_프로그래머스_3
    lv1

---
+++ MH/Solution230318_2.java
@@ -0,0 +1,23 @@
+public class Solution230318_2 {
+	
+	// 프로그래머스 - lv.1 - 평균 구하기
+	public double solution(int[] arr) {
+        double answer = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+        	answer += arr[i];
+        }
+        
+        return answer/arr.length;
+    }
+	
+	public static void main(String[] args) {
+		int[] ex1 = {1, 2, 3, 4};
+		int[] ex2 = {5, 5};
+		
+		Solution230318_2 s = new Solution230318_2(); 
+		double result = s.solution(ex2);
+		
+		System.out.println(result);
+	}
+}
