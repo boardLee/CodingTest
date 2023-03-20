ff059eaa4b760e4947167cc29b343993ed67c81e
MHLEE12 <coolmh12@gmail.com>
2023-03-20 21:22:53 +0900

    230320 코테 연습 5
    프로그래머스 lv.0

---
+++ MH/Solution230320_5.java
@@ -0,0 +1,10 @@
+public class Solution230320_5 {
+	
+	// 프로그래머스 - lv.0 - 두 수의 곱(베리 이지..)
+	
+	public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        
+        return answer;
+    }
+}
