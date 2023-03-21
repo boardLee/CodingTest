6c8b5122d1ff45f5208af2f534802b7ac6d96cb6
MHLEE12 <coolmh12@gmail.com>
2023-03-21 22:34:16 +0900

    230321 코테 문제2

---
+++ MH/Solution230321_5.java
@@ -0,0 +1,20 @@
+public class Solution230321_5 {
+	
+	// 프로그래머스 - lv.1 - 나머지가 1이 되는 수 찾기
+	public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2; i <= n; i++) {
+        	if(n % i == 1) {
+        		return i;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		Solution230321_5 s = new Solution230321_5();
+		System.out.println(s.solution(12));
+	}
+}
