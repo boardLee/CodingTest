f9c89afd232154711636091328e7dfe816ae8bf4
Jung-Kwon Lee <akdirdl@gmail.com>
2023-03-23 23:49:47 +0900

    230323_sol1

---
+++ jk/CodingTest/src/jk/programmers/day_230323/Sol_1.java
@@ -0,0 +1,22 @@
+package jk.programmers.day_230323;
+
+public class Sol_1 {
+    public int solution(String A, String B) {
+        int answer = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.equals(B)) {
+                return answer;
+            }
+            String a = A.substring(A.length() - 1);
+            A = a + A.substring(0, A.length() - 1);
+            answer++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Sol_1 s1 = new Sol_1();
+        System.out.println(s1.solution("hello", "ohell"));
+
+    }
+}
