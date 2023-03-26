bc8053dd270a893cda5419ea899bb2fee5713d72
MHLEE12 <coolmh12@gmail.com>
2023-03-26 16:06:42 +0900

    230326_ 03/22 코테 문제 풂

--- MH/mar/Solution230322_2.java
+++ MH/mar/Solution230322_2.java
@@ -1,14 +1,15 @@
 package mar;
 
 public class Solution230322_2 {
+	// 프로그래머스 - lv.0 - 두 수의 나눗셈
 	public int solution(int num1, int num2) {
-        int answer = Integer.valueOf((int) ((Long.valueOf(num1) / Long.valueOf(num2)) * 1000));
+        int answer = (int) ((Double.valueOf(num1) / Double.valueOf(num2)) * 1000);
         
         return answer;
     }
 	
 	public static void main(String[] args) {
 		Solution230322_2 s = new Solution230322_2();
-		System.out.println(s.solution(3, 2));
+		System.out.println(s.solution(7, 3));
 	}
 }
