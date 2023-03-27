8ff6e7eb3207c756f889f24238b625afd9c07583
MHLEE12 <coolmh12@gmail.com>
2023-03-27 22:02:28 +0900

    230327 코테연습

---
+++ MH/mar/Solution230327_1.java
@@ -0,0 +1,9 @@
+package mar;
+
+public class Solution230327_1 {
+	public int solution(int slice, int n) {
+        int answer = (n % slice == 0 ? (n / slice) : (n / slice) + 1);
+        
+        return answer;
+    }
+}
